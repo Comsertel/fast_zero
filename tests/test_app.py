02317af34753e41de40c70bc0265bee31d9a40cc
marcelo.farinaro <marcelo.farinaro@quintoandar.com.br>
2024-06-25 09:57:04 -0300

    More implementations and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -29,3 +29,33 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': 'senha Top',
+            'username': 'marcelo',
+            'email': 'marcelo@example.com',
+        })
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+                'username': 'marcelo',
+                'email': 'marcelo@example.com',
+                'id': 1,
+            }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
