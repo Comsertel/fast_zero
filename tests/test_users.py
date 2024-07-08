2bba475459a766eb895eba0072160c24a9a8b761
marcelo.farinaro <marcelo.farinaro@quintoandar.com.br>
2024-07-08 14:02:57 -0300

    Aula 7

---
+++ tests/test_users.py
@@ -0,0 +1,103 @@
+from http import HTTPStatus
+
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users_empty(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    print(42)
+    print(response.json())
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/1')
+    user_schema
+    assert response.json() == user_schema
+
+
+def test_read_user_without_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': user.id,
+    }
+
+
+def test_update_user_not_found(client, user, token):
+    response = client.put(
+        '/users/100',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'password': 'senha Top',
+            'username': 'marcelo',
+            'email': 'marcelo@example.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client, token):
+    response = client.delete(
+        '/users/100',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_wrong_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id + 1}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code  # == HTTPStatus.FORBIDDEN
