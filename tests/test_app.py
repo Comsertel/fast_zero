6fbfa3cad5bcd4b9070e1ef82654cbfa1603d655
marcelo.farinaro <marcelo.farinaro@quintoandar.com.br>
2025-06-11 08:24:29 -0300

    Aulas 2 + quiz

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,9 @@ def test_root_deve_retornar_42():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': '42'}
+
+
+def test_root_html_deve_retornar_html():
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
